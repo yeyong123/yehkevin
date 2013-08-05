13d11dfc7aed9f9241736d82f309d6a8c20cde1c
yeyong14 <yeyong14@gmail.com>
2013-08-05 17:22:27 +0800

    初始化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yehkevin::Application.config.secret_token = '067a8e2d53cf10e0ea5445897596164fd548bad88704574e2c79bbe5889c46b14b21ce521eaf674db873278c685a85f34acfa132cd80d7ccc20e1cd350196e5f'
