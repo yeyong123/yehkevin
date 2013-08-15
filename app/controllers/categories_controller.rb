9f183f6707ac6421696325fe964243efdf3e3f46
yeyong14 <yeyong14@gmail.com>
2013-08-15 17:35:40 +0800

    加了面包屑

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,15 +3,10 @@ class CategoriesController < ApplicationController
   end
 
   def show
-		begin
 		@category = Category.find(params[:id])
 		@tag = Tag.find(params[:id])
-		if @products.nil?
-			flash[:notice] = "这个真没有"
-			redirect_to home_path
-		else
-			render 'show'
-		end
 	end
-	end
+
+
+	
 end
